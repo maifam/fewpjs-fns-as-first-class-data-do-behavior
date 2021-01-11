0b48711228b1251dd579fd814da8dadf44eb4c83
Mai Pham <maitphm@gmail.com>
2021-01-10 21:22:33 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+
+  if(parseInt(time) < 12) {
+    return "Good Morning"
+  } else if(parseInt(time) > 17){
+    return "Good Evening"
+  } else{
+    return "Good Afternoon"
+  } 
+}
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(str) { 
+  document.getElementById('greeting').innerText = str;
+}
